fbe68edac470e781e2995a4b67a3723a41f1a9b2
Yury Prokashev <yury.prokashev@gmail.com>
2019-10-01 08:51:25 +0300

    insert and update 25% faster

--- money/ObjectTableApp.js
+++ money/ObjectTableApp.js
@@ -6,7 +6,7 @@ function ObjectTableAppFactory() {
         var _sheetUrl;
         var _tableName;
         var _objects;
-        var _headerIndexes;
+        var _headerIndex;
         var _Constructor;
         var _objectIndexById;
         var _rowIndexByObjectId;
@@ -50,9 +50,9 @@ function ObjectTableAppFactory() {
             _sheet = SpreadsheetApp.openByUrl(_sheetUrl).getSheetByName(_tableName);
             _sheet.getDataRange().getValues().forEach(function (row, rowIndex) {
                 if(rowIndex === 0){
-                    _headerIndexes = IndexApp.createOneToOneIndex();
+                    _headerIndex = IndexApp.createOneToOneIndex();
                     row.forEach(function(objectPropertyName, colIndex){
-                        _headerIndexes.set(objectPropertyName, colIndex);
+                        _headerIndex.set(objectPropertyName, colIndex);
                     });
                 }
                 if(row[0] !== "" && rowIndex > 0){
@@ -60,19 +60,31 @@ function ObjectTableAppFactory() {
                     Object.keys(object).forEach(function (objectFunctionName) {
                         if(objectFunctionName.indexOf("set") === 0){
                             var objectPropertyName = _objectFunctionNameToObjectPropertyName(objectFunctionName);
-                            var objectPropertyColumnIndex = _headerIndexes.get(objectPropertyName);
+                            var objectPropertyColumnIndex = _headerIndex.get(objectPropertyName);
                             object[objectFunctionName](row[objectPropertyColumnIndex]);
                         }
                     });
-                    _indexObject(object, rowIndex);
+                    _addObjectToIndex(object, rowIndex);
                 }
             })
         }
 
-        function _indexObject(object, rowIndex) {
+        function _addObjectToIndex(object, rowIndex) {
             _objects.push(object);
             _objectIndexById.set(object.getId(), object);
             _rowIndexByObjectId.set(object.getId(), rowIndex);
+        }
+
+        function _removeObjectFromIndex(object){
+            var objectIndex;
+            _objects.forEach(function (item, index){
+                if(item.getId() === object.getId()){
+                    objectIndex = index;
+                }
+            });
+            _objects.splice(objectIndex, 1);
+            _objectIndexById.remove(object.getId());
+            _rowIndexByObjectId.set(object.getId());
         }
 
         function _saveOne(object){
@@ -105,38 +117,30 @@ function ObjectTableAppFactory() {
         function _removeOne(object) {
             var objectRowIndex = _rowIndexByObjectId.get(object.getId());
             _sheet.deleteRow(objectRowIndex);
-            var objectIndex;
-            _objects.forEach(function (item, index){
-                if(item.getId() === object.getId()){
-                    objectIndex = index;
-                }
-            });
-            _objects.splice(objectIndex, 1);
-            _objectIndexById.remove(object.getId());
-            _rowIndexByObjectId.set(object.getId());
+            _removeObjectFromIndex(object);
         }
 
         function _insertOne(object) {
             var objectRow = _createObjectRow(object);
-            _sheet.appendRow(_objectRowToGoogleSheetRow(objectRow));
+            _sheet.appendRow(objectRow);
             var appendedRowIndex = _sheet.getLastRow();
-            _indexObject(object, appendedRowIndex);
+            _addObjectToIndex(object, appendedRowIndex);
         }
 
         function _insertBatch(objects) {
             var rangeValues = [];
             objects.forEach(function (object) {
                 var objectRow = _createObjectRow(object);
-                rangeValues.push(_objectRowToGoogleSheetRow(objectRow));
+                rangeValues.push(objectRow);
             });
             if(objects.length > 0){
                 var lastRowIndex = _sheet.getLastRow();
                 _sheet.insertRowsAfter(lastRowIndex, objects.length);
-                _sheet.getRange(lastRowIndex + 1, 1, objects.length, _headerIndexes.keys().length).setValues(rangeValues);
+                _sheet.getRange(lastRowIndex + 1, 1, objects.length, _headerIndex.keys().length).setValues(rangeValues);
 
                 var newObjectCount = 0;
                 while(newObjectCount < objects.length){
-                    _indexObject(objects[newObjectCount], lastRowIndex + newObjectCount + 1);
+                    _addObjectToIndex(objects[newObjectCount], lastRowIndex + newObjectCount + 1);
                     newObjectCount++;
                 }
             }
@@ -144,10 +148,10 @@ function ObjectTableAppFactory() {
 
         function _updateOne(object) {
             var objectRowIndex = _rowIndexByObjectId.get(object.getId());
-            var objectRange = _sheet.getRange(objectRowIndex, 1, 1, _headerIndexes.keys().length);
+            var objectRange = _sheet.getRange(objectRowIndex, 1, 1, _headerIndex.keys().length);
             var objectRow = _createObjectRow(object);
-            objectRange.setValues([_objectRowToGoogleSheetRow(objectRow)]);
-            _indexObject(object, objectRowIndex);
+            objectRange.setValues([objectRow]);
+            _addObjectToIndex(object, objectRowIndex);
         }
 
         function _updateBatch(objects) {
@@ -162,37 +166,16 @@ function ObjectTableAppFactory() {
             return objectPropertyName;
         }
 
+        function _objectPropertyNameToObjectFunctionName(objectPropertyName) {
+            var objectFunctionName = objectPropertyName.charAt(0).toUpperCase() + objectPropertyName.substring(1);
+            return "get" + objectFunctionName;
+        }
+
         function _createObjectRow(object){
-            var objectRow = new ObjectRow();
-            Object.keys(object).forEach(function(objectFunctionName){
-                if(objectFunctionName.indexOf("get") === 0){
-                    var objectPropertyName = _objectFunctionNameToObjectPropertyName(objectFunctionName);
-                    var propertyColumnIndex = _headerIndexes.get(objectPropertyName);
-                    objectRow.addValue(propertyColumnIndex, object[objectFunctionName]());
-                }
-            });
-            return objectRow;
-        }
-
-        function ObjectRow(){
-            var _valueIndex = IndexApp.createOneToOneIndex();
-            this.addValue = function (columnIndex, value) {
-                _valueIndex.set(columnIndex, value);
-            };
-            this.getValue = function (columnIndex) {
-                return _valueIndex.get(columnIndex);
-            };
-            this.getColumnIndexes = function () {
-                return _valueIndex.keys();
-            }
-        }
-
-        function _objectRowToGoogleSheetRow(objectRow) {
             var row = [];
-            objectRow.getColumnIndexes().sort(function (a, b) {
-                return parseInt(a) - parseInt(b);
-            }).forEach(function(columnIndex){
-                row.push(objectRow.getValue(columnIndex));
+            _headerIndex.keys().forEach(function(objectPropertyName){
+                var objectFunctionName = _objectPropertyNameToObjectFunctionName(objectPropertyName);
+                row.push(object[objectFunctionName]());
             });
             return row;
         }
