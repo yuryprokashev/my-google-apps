59f8c2305629adba6be2ef2e41b859550b831728
Yury Prokashev <yury.prokashev@gmail.com>
2019-11-12 21:49:41 +0300

    fixing bug with empty object mapping rule collection

--- test/TestPropertyMapper.js
+++ test/TestPropertyMapper.js
@@ -6,16 +6,8 @@
  * @constructor
  */
 function TestPropertyMapper(baseModule, mappingModule, testModule) {
-    var indexBySourceObjectType = baseModule.IndexFactory.getBuilder()
-        .setStorageMap(baseModule.MapFactory.getBuilder().build())
-        .setIndexedPropertyName("getSourceObjectType")
-        .build();
-    var emptyCollection = baseModule.CollectionFactory.getBuilder()
-        .setInstances([])
-        .addPropertyIndex(indexBySourceObjectType)
-        .build();
     var propertyMapper = mappingModule.PropertyMapperFactory.getBuilder()
-        .setObjectMappingRuleCollection(emptyCollection)
+        .setObjectMappingRuleCollection(testModule.objectMappingRuleCollection)
         .build();
     this.map = function (propertyMappingRule, sourceObject) {
         return propertyMapper.map(propertyMappingRule, sourceObject);
