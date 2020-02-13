e3bf850248181d6afaf6b5b3afe3ce991e7a07b6
Yury Prokashev <yury.prokashev@gmail.com>
2020-02-13 09:17:13 +0300

    moved trades mapping definition from TradesModule to TradesModule.TradesApp

--- trades/FinnhubTradeApp.js
+++ trades/FinnhubTradeApp.js
@@ -5,6 +5,8 @@
  * @constructor
  */
 function FinnhubTradeAppConstructor(BaseModule, FinnhubTradesModule){
+    var _tradeToRowMap;
+    var _tradesFactory;
     var _tradesTable;
     var tradesSpreadsheetUrl = "https://docs.google.com/spreadsheets/d/1bOF0xfyD6ssvCAY1_R3n76TLqNupOxkm6g9H4SFb0Tk/edit";
     var tradesTabName = "FinnhubTrades";
@@ -14,10 +16,18 @@ function FinnhubTradeAppConstructor(BaseModule, FinnhubTradesModule){
         _tradesTable.save(finnhubTradeRows);
     };
     function _init(){
+        _tradeToRowMap = BaseModule.KeyMapBuilderFactory.getBuilder().build();
+        _tradeToRowMap.set("t", 0);
+        _tradeToRowMap.set("s", 1);
+        _tradeToRowMap.set("p", 2);
+        _tradeToRowMap.set("v", 3);
         _tradesTable = BaseModule.TableBuilderFactory.getBuilder()
             .setSpreadsheetUrl(tradesSpreadsheetUrl)
             .setSheetName(tradesTabName)
-            .setNumberOfColumns(4)
+            .setNumberOfColumns(_tradeToRowMap.keys().length)
+            .build();
+        _tradesFactory = BaseModule.ObjectToRowFactoryBuilderFactory.getBuilder()
+            .setObjectToRowMap(_tradeToRowMap)
             .build();
     }
 }
--- trades/TradesModule.js
+++ trades/TradesModule.js
@@ -4,13 +4,5 @@
  */
 function TradeModuleConstructor() {
     var baseModule = new BaseModule.BaseModuleConstructor();
-    var tradeToRowMap = baseModule.KeyMapBuilderFactory.getBuilder().build();
-    tradeToRowMap.set("t", 0);
-    tradeToRowMap.set("s", 1);
-    tradeToRowMap.set("p", 2);
-    tradeToRowMap.set("v", 3);
-    this.FinnhubTradeToRowFactory = baseModule.ObjectToRowFactoryBuilderFactory.getBuilder()
-        .setObjectToRowMap(tradeToRowMap)
-        .build();
     this.FinnhubTradeApp = new FinnhubTradeAppConstructor(baseModule, this);
 }
