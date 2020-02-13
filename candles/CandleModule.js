d28a5c75531d5f95983f7f78e20657ca1f734795
Yury Prokashev <yury.prokashev@gmail.com>
2020-02-13 08:49:12 +0300

    using ObjectToRowFactoryBuilder in Candles

--- candles/CandleModule.js
+++ candles/CandleModule.js
@@ -5,8 +5,16 @@
 function CandleModuleConstructor() {
     var baseModule = new BaseModule.BaseModuleConstructor();
     var httpModule = new HttpModule.HttpModuleConstructor();
+    var candleToRowMap = baseModule.KeyMapBuilderFactory.getBuilder().build();
+    candleToRowMap.set("getTimestamp", 0);
+    candleToRowMap.set("getSymbol", 1);
+    candleToRowMap.set("getOpenPrice", 2);
+    candleToRowMap.set("getHighestPrice", 3);
+    candleToRowMap.set("getLowestPrice", 4);
+    candleToRowMap.set("getClosePrice", 5);
+    candleToRowMap.set("getVolume", 6);
     this.CandleBuilderFactory = new CandleBuilderFactoryConstructor(baseModule, this);
     this.CandleApp = new CandleAppConstructor(baseModule, this);
-    this.CandleRowFactory = new CandleRowFactoryConstructor(baseModule, this);
+    this.CandleRowFactory = baseModule.ObjectToRowFactoryBuilderFactory.getBuilder().setObjectToRowMap(candleToRowMap).build();
     this.CandleSynchronizer = new CandleSynchronizerConstructor(baseModule, httpModule, this);
 }
